ab252bd8fadd8511036e318d4b22c74343eb6955
gabyKOKAI <gaby@kokai.com.mx>
2018-04-08 17:20:55 -0500

    email

--- resources/views/emails/contacto.blade.php
+++ resources/views/emails/contacto.blade.php
@@ -1,5 +1,13 @@
-<html>
-<head></head>
+<!DOCTYPE html>
+<head>
+	<meta charset='utf-8'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href='{{URL::asset('/css/generico.css')}}' type='text/css' rel='stylesheet'>
+	<link href='{{URL::asset('/css/style.css')}}' type='text/css' rel='stylesheet'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+	<script src="{{URL::asset('/css/Bootstrap/js/bootstrap.min.js')}}"></script>
+</head>
 <body style="background: white; color: black">
 <h1 class="center">Información de Contacto de la página</h1>
 <div class="container">
