0ffd8c66b02f64edb0a90007e87a1e2b70d22a15
gabyKOKAI <gaby@kokai.com.mx>
2018-04-09 18:41:57 -0500

    hola

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -11,11 +11,11 @@ class MasterController extends Controller
     {
         //ContactUS::create($request->all());
 
-        $contactName =  hola;//urldecode($request->input('nombre'));
-        $contactEmail = hola;// urldecode($request->input('correo'));
-        $contactCel =  hola;//urldecode($request->input('celular'));
-        $contactTel =  hola;//urldecode($request->input('telefono'));
-        $contactMessage =  hola;//urldecode($request->input('mensaje'));
+        $contactName =  "hola";//urldecode($request->input('nombre'));
+        $contactEmail = "hola";// urldecode($request->input('correo'));
+        $contactCel =  "hola";//urldecode($request->input('celular'));
+        $contactTel =  "hola";//urldecode($request->input('telefono'));
+        $contactMessage =  "hola";//urldecode($request->input('mensaje'));
 
 
         $data = array('nombre'=>$contactName, 'correo'=>$contactEmail, 'celular'=>$contactCel, 'telefono'=>$contactTel, 'mensaje'=>$contactMessage);
