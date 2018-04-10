333faf70ff1b947546a995f5b98f13e80aab43f0
gabyKOKAI <gaby@kokai.com.mx>
2018-04-09 22:14:01 -0500

    Servicios

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -9,10 +9,10 @@
     </div>
     <div class"row">
         <div class="containerServ col-md-6">
-            <img src="{{URL::asset('/images/kokai_alberca.jpg')}}" class="imgServ img-responsive " alt="" >
-            <div class="middleServ1 hidden-sm hidden-xs">
+            <img src="{{URL::asset('/images/kokai_alberca.png')}}" class="imgServ img-responsive " alt="" >
+            <!--div class="middleServ1 hidden-sm hidden-xs">
                 <div class="textServ1">LA ALBERCA</div>
-            </div>
+            </div-->
              <div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">LA ALBERCA</h1>
@@ -20,7 +20,8 @@
                 </div>
             </div>
             <div class="middleServ hidden-sm hidden-xs">
-                <div class="textServ">
+                <img src="{{URL::asset('/images/kokai_albercaBack.png')}}" class="img-responsive " alt="" >
+                <!--div class="textServ">
                     <P><br></p>
                     <p class="font50"><br></p>
                     <h1 class="bold">LA ALBERCA</h1>
@@ -35,15 +36,15 @@
                         </div>
                     </div>
                     <p class="font50"><br></p>
-                </div>
+                </div-->
             </div>
         </div>
        <div class="hidden-lg hidden-md col-sm-12 col-xs-12"><br></div>
        <div class="containerServ col-md-6">
-            <img src="{{URL::asset('/images/kokai_restaurante.jpg')}}" class="imgServ img-responsive " alt="" >
-            <div class="middleServ1 hidden-sm hidden-xs">
+            <img src="{{URL::asset('/images/kokai_restaurante.png')}}" class="imgServ img-responsive " alt="" >
+            <!--div class="middleServ1 hidden-sm hidden-xs">
                 <div class="textServ1">EL RESTAURANTE</div>
-            </div>
+            </div-->
              <div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">MESÓN ESPAÑOL</h1>
@@ -53,13 +54,14 @@
                 </div>
             </div>
             <div class="middleServ hidden-sm hidden-xs">
-                <div class="textServ">
+                <img src="{{URL::asset('/images/kokai_restauranteBack.png')}}" class="img-responsive " alt="" >
+                <!--div class="textServ">
                     <P><br></p>
                     <h1 class="bold">MESÓN ESPAÑOL</h1>
                     <P>Exquisita y auténtica cocina  tradicional española.</p>
                     <P>&ensp;&ensp; &ensp; &ensp; Desayunos &ensp; &ensp; &ensp; &ensp; 7:00am - 12:00pm</p>
                     <P>&ensp;&ensp; &ensp; &ensp;&ensp; Comidas &ensp;&ensp;&ensp;&ensp; &ensp; 12:00pm - 10:00pm</p>
-                </div>
+                </div-->
             </div>
         </div>
     </div>
@@ -70,31 +72,32 @@
     </div>
     <div class"row">
         <div class="containerServ col-md-6">
-            <img src="{{URL::asset('/images/kokai_bar.jpg')}}" class="imgServ img-responsive " alt="" >
+            <img src="{{URL::asset('/images/kokai_bar.png')}}" class="imgServ img-responsive " alt="" >
             <div class="middleServ1 hidden-sm hidden-xs">
-                <div class="textServ1">EL BAR</div>
+                <!--div class="textServ1">EL BAR</div-->
             </div>
             <div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">EL BAR</h1>
-                    <P>Ven a disfrutar una copa en compañia de tus amigos y divertirse viendo deportes, jugar dominó o cartas.</p>
+                    <P>Ven a disfrutar una copa en compañia de tus amigos y</P><P> divertirse viendo deportes, jugar dominó o cartas.</p>
                     <P>&ensp;&ensp; Lunes a Sabados &ensp; &ensp; 04:00pm - 1:00am</p>
                 </div>
             </div>
             <div class="middleServ hidden-sm hidden-xs">
-                   <div class="textServ">
+               <img src="{{URL::asset('/images/kokai_barBack.png')}}" class="img-responsive " alt="" >
+               <!--div class="textServ">
                     <P><br></p>
                     <h1 class="bold">EL BAR</h1>
                     <P>Ven a disfrutar una copa en compañia de tus amigos y divertirse viendo deportes, jugar dominó o cartas.</p>
                     <P>&ensp;&ensp; Lunes a Sabados &ensp; &ensp; 04:00pm - 1:00am</p>
-                </div>
+                </div-->
             </div>
         </div>
         <div class="hidden-lg hidden-md col-sm-12 col-xs-12"><br></div>
          <div class="containerServ col-md-6">
-            <img src="{{URL::asset('/images/kokai_camarote.jpg')}}" class="imgServ img-responsive " alt="" >
+            <img src="{{URL::asset('/images/kokai_camarote.png')}}" class="imgServ img-responsive " alt="" >
             <div class="middleServ1 hidden-sm hidden-xs">
-                <div class="textServ1">EL CAMAROTE</div>
+                <!--div class="textServ1">EL CAMAROTE</div-->
             </div>
             <div class="hidden-lg hidden-md">
                 <div class="center">
@@ -105,13 +108,14 @@
                 </div>
             </div>
             <div class="middleServ hidden-sm hidden-xs">
-                <div class="textServ">
+                <img src="{{URL::asset('/images/kokai_camaroteBack.png')}}" class="img-responsive " alt="" >
+                <!--div class="textServ">
                     <P><br></p>
                     <h1 class="bold">EL CAMAROTE</h1>
                     <P>Excelente lugar para disfrutar de shows continuos con una gran variedad de artistas.</p>
                     <P>Viernes y Sabados</p>
                     <P>10:30pm - 03:00am</p>
-                </div>
+                </div-->
             </div>
         </div>
     </div>
