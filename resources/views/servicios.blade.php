5b856cbe39bdbee4a7d5532572de73611ea1fb54
gabyKOKAI <gaby@kokai.com.mx>
2018-04-09 22:19:20 -0500

    Servicios celular

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -45,15 +45,15 @@
             <!--div class="middleServ1 hidden-sm hidden-xs">
                 <div class="textServ1">EL RESTAURANTE</div>
             </div-->
-             <div class="hidden-lg hidden-md">
+             <!--div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">MESÓN ESPAÑOL</h1>
                     <P>Exquisita y auténtica cocina  tradicional española.</p>
                     <P>&ensp;&ensp; &ensp; &ensp; Desayunos &ensp; &ensp; &ensp; &ensp; 7:00am - 12:00pm</p>
                     <P>&ensp;&ensp; &ensp; &ensp;&ensp; Comidas &ensp;&ensp;&ensp;&ensp; &ensp; 12:00pm - 10:00pm</p>
                 </div>
-            </div>
-            <div class="middleServ hidden-sm hidden-xs">
+            </div-->
+            <div class="middleServ">
                 <img src="{{URL::asset('/images/kokai_restauranteBack.png')}}" class="img-responsive " alt="" >
                 <!--div class="textServ">
                     <P><br></p>
@@ -76,14 +76,14 @@
             <div class="middleServ1 hidden-sm hidden-xs">
                 <!--div class="textServ1">EL BAR</div-->
             </div>
-            <div class="hidden-lg hidden-md">
+            <!--div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">EL BAR</h1>
                     <P>Ven a disfrutar una copa en compañia de tus amigos y</P><P> divertirse viendo deportes, jugar dominó o cartas.</p>
                     <P>&ensp;&ensp; Lunes a Sabados &ensp; &ensp; 04:00pm - 1:00am</p>
                 </div>
-            </div>
-            <div class="middleServ hidden-sm hidden-xs">
+            </div-->
+            <div class="middleServ">
                <img src="{{URL::asset('/images/kokai_barBack.png')}}" class="img-responsive " alt="" >
                <!--div class="textServ">
                     <P><br></p>
@@ -99,15 +99,15 @@
             <div class="middleServ1 hidden-sm hidden-xs">
                 <!--div class="textServ1">EL CAMAROTE</div-->
             </div>
-            <div class="hidden-lg hidden-md">
+            <!--div class="hidden-lg hidden-md">
                 <div class="center">
                     <h1 class="bold">EL CAMAROTE</h1>
                     <P>Excelente lugar para disfrutar de shows continuos con una gran variedad de artistas.</p>
                     <P>Viernes y Sabados</p>
                     <P>10:30pm - 03:00am</p>
                 </div>
-            </div>
-            <div class="middleServ hidden-sm hidden-xs">
+            </div-->
+            <div class="middleServ">
                 <img src="{{URL::asset('/images/kokai_camaroteBack.png')}}" class="img-responsive " alt="" >
                 <!--div class="textServ">
                     <P><br></p>
