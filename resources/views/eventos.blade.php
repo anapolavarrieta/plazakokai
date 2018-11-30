26cd456a7827190b9be87611436ab139be4f290c
gabyKOKAI <gaby@kokai.com.mx>
2018-11-30 11:36:06 -0600

    Evento Teleton

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -8,14 +8,19 @@
         </div>
     </div>
     <div class"row">
-       <div class="containerServ col-md-1">
+       <div class="containerServ col-md-0">
        </div>
-       <div class="containerServ col-md-10">
+       <div class="containerServ col-md-6">
+            <div class="parent">
+                <img src="{{URL::asset('/images/teleton_diciembre_2018.jpg')}}" class="img-responsive image1" alt="" >
+            </div>
+        </div>
+       <div class="containerServ col-md-6">
             <div class="parent">
                 <img src="{{URL::asset('/images/año_nuevo_2018.png')}}" class="img-responsive image1" alt="" >
             </div>
         </div>
-       <div class="containerServ col-md-1">
+       <div class="containerServ col-md-0">
         </div>
     </div>
     <div class"row">
